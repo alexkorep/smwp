836a6f34a57a34c4784d230b1c153506ef5a4ed4
Alexey Korepanov <alexkorep@gmail.com>
2016-02-04 21:03:54 +0100

    archive - post titles linked to post page

--- archive.php
+++ archive.php
@@ -16,10 +16,12 @@
 
       <?php
           while (have_posts()): the_post();
+            if ( is_single() ) :
+              the_title( '<h1 class="entry-title">', '</h1>' );
+            else :
+              the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+            endif;
       ?>
-
-      <h2><?php the_title();?></h2>
-
       <div><?php the_content(__('(дальше...)'));?></div>
       <hr>
       <?php
