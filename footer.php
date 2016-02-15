7d91aac2b72d31d1c804fc5e45c292265bc9120b
Alexey Korepanov <alexkorep@gmail.com>
2016-02-15 20:15:32 +0100

    blue ui

--- footer.php
+++ footer.php
@@ -1,31 +1,29 @@
-          </div>
+  </div>
+</div>
+
+<div class="container-full">
+  <div class="row">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 col-sm-4 col-xs-12 col-lg-4 m-t-md">
+            <?php if (is_active_sidebar('bottom_left')): ?>
+                <?php dynamic_sidebar('bottom_left');?>
+            <?php endif;?>
+        </div>
+        <div class="col-md-4 col-sm-4 col-xs-12 col-lg-4 m-t-md">
+            <?php if (is_active_sidebar('bottom_center')): ?>
+                <?php dynamic_sidebar('bottom_center');?>
+            <?php endif;?>
+        </div>
+        <div class="col-md-4 col-sm-4 col-xs-12 col-lg-4 m-t-md">
+            <?php if (is_active_sidebar('bottom_right')): ?>
+                <?php dynamic_sidebar('bottom_right');?>
+            <?php endif;?>
         </div>
       </div>
     </div>
-
-
-
-    <div class="section">
-      <div class="container">
-        <div class="row smwp-toprow">
-          <div class="col-md-4 col-sm-4 col-xs-12 col-lg-4 m-t-md">
-              <?php if (is_active_sidebar('bottom_left')): ?>
-                  <?php dynamic_sidebar('bottom_left');?>
-              <?php endif;?>
-          </div>
-          <div class="col-md-4 col-sm-4 col-xs-12 col-lg-4 m-t-md">
-              <?php if (is_active_sidebar('bottom_center')): ?>
-                  <?php dynamic_sidebar('bottom_center');?>
-              <?php endif;?>
-          </div>
-          <div class="col-md-4 col-sm-4 col-xs-12 col-lg-4 m-t-md">
-              <?php if (is_active_sidebar('bottom_right')): ?>
-                  <?php dynamic_sidebar('bottom_right');?>
-              <?php endif;?>
-          </div>
-        </div>
-      </div>
-    </div>
+  </div>
+</div>
 
     <?php wp_footer(); ?>
   </body>
--- header.php
+++ header.php
@@ -14,7 +14,33 @@
 
   <body <?php body_class(); ?> style="font-family: 'Ubuntu', sans-serif;">
 
-    <!-- Menu -->
+
+
+<div class="container-full">
+  <div class="row">
+    <div class="col-lg-12">
+
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-2 col-sm-3">
+            <?php if ( get_theme_mod( 'themeslug_logo' ) ) : ?>
+              <div class='site-logo'>
+                <a href='<?php echo esc_url( home_url( '/' ) ); ?>'
+                  title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'
+                  rel='home'>
+                  <img src="<?php echo esc_url( get_theme_mod( 'themeslug_logo' ) ); ?>"
+                    alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"
+                    class="center-block img-fluid m-y-md img-circle img-responsive" />
+                </a>
+              </div>
+            <?php endif; ?>
+          </div>
+          <div class="col-lg-10 col-sm-9">
+            <h1 class="display-4 m-t-md pi-item"><?php bloginfo( 'name' ); ?></h1>
+            <p class="lead pi-item"><?php echo get_bloginfo( 'description', 'display' ); ?></p>
+
+
+<!-- Menu -->
     <div class="section">
       <div class="container">
         <div class="row">
@@ -36,31 +62,17 @@
       </div>
     </div>
 
-    <div class="section">
-      <div class="container">
-        <div class="row smwp-toprow">
-          <div class="col-lg-2 col-sm-3">
-            <?php if ( get_theme_mod( 'themeslug_logo' ) ) : ?>
-              <div class='site-logo'>
-                <a href='<?php echo esc_url( home_url( '/' ) ); ?>'
-                  title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'
-                  rel='home'>
-                  <img src="<?php echo esc_url( get_theme_mod( 'themeslug_logo' ) ); ?>"
-                    alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"
-                    class="center-block img-fluid m-t-md" />
-                </a>
-              </div>
-            <?php endif; ?>
-          </div>
-          <div class="col-lg-10 col-sm-9">
-            <h1 class="display-4 m-t-md pi-item"><?php bloginfo( 'name' ); ?></h1>
-            <p class="lead pi-item"><?php echo get_bloginfo( 'description', 'display' ); ?></p>
+
           </div>
         </div>
       </div>
 
+
     </div>
-    <div class="p-y-lg section">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
+  </div>
+</div>
+<!-- /container full -->
+
+<div class="container">
+  <hr />
+  <div class="row">
