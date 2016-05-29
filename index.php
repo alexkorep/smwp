b0d99fbb1b3c0b21d363fb4bf13f2ecf521c6802
Alexey Korepanov <alexkorep@gmail.com>
2016-05-29 16:18:54 +0200

    right panel on every page

--- index.php
+++ index.php
@@ -2,19 +2,34 @@
 
   <div id="main">
     <div id="content">
-      <?php
-        if (have_posts()): while (have_posts()): the_post();
-      ?>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-sm-12 col-xs-12 col-lg-6">
 
-      <h1><?php the_title();?></h1>
 
-      <div><?php the_content(__('(more...)'));?></div>
-      <hr>
-      <?php
-        endwhile;else:
-      ?>
-        <p><?php _e('Sorry, no posts matched your criteria.');?></p>
-      <?php endif;?>
+            <?php
+              if (have_posts()): while (have_posts()): the_post();
+            ?>
+
+            <h1><?php the_title();?></h1>
+
+            <div><?php the_content(__('(more...)'));?></div>
+            <hr>
+            <?php
+              endwhile;else:
+            ?>
+              <p><?php _e('Sorry, no posts matched your criteria.');?></p>
+            <?php endif;?>
+
+
+          </div>
+          <div class="col-md-6 col-sm-12 col-xs-12 col-lg-6">
+              <?php if (is_active_sidebar('home_right_1')): ?>
+                  <?php dynamic_sidebar('home_right_1');?>
+              <?php endif;?>
+          </div>
+        </div>
+      </div>      
     </div>
   </div>
 <?php get_footer();?>
--- page.php
+++ page.php
@@ -2,28 +2,43 @@
 
   <div id="main">
     <div id="content">
-      <?php the_title( '<h1>', '</h1>' ); ?>
+      
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-sm-12 col-xs-12 col-lg-6">
+              
+              
+            <?php the_title( '<h1>', '</h1>' ); ?>
 
-      <div>
-        <?php
-          while ( have_posts() ) : the_post();
-            the_content();
-            $post_id = get_the_ID();
-            $product_buy_link = get_post_meta($post_id, 'product_buy_link', true);
-            if ($product_buy_link) {
-              $product_buy_title = get_post_meta($post_id, 'product_buy_title', true);
-              if (!$product_buy_title) {
-                $product_buy_title = 'Купить';
-              }
+            <div>
+              <?php
+                while ( have_posts() ) : the_post();
+                  the_content();
+                  $post_id = get_the_ID();
+                  $product_buy_link = get_post_meta($post_id, 'product_buy_link', true);
+                  if ($product_buy_link) {
+                    $product_buy_title = get_post_meta($post_id, 'product_buy_title', true);
+                    if (!$product_buy_title) {
+                      $product_buy_title = 'Купить';
+                    }
 
-              echo '<a href="' . $product_buy_link . '" class="btn btn-primary">' .
-                $product_buy_title . '</a>';
-            }
-          endwhile;
-        ?>
+                    echo '<a href="' . $product_buy_link . '" class="btn btn-primary">' .
+                      $product_buy_title . '</a>';
+                  }
+                endwhile;
+              ?>
+            </div>
+              
+              
+          </div>
+          <div class="col-md-6 col-sm-12 col-xs-12 col-lg-6">
+              <?php if (is_active_sidebar('home_right_1')): ?>
+                  <?php dynamic_sidebar('home_right_1');?>
+              <?php endif;?>
+          </div>
+        </div>
       </div>
-
-
+            
     </div>
   </div>
 
