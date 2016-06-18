f061465c6380081382c7d94ee6a184d0bcb8b3d6
Alexey Korepanov <alexkorep@gmail.com>
2016-06-18 16:20:02 +0200

    changed comments widget settings

--- page.php
+++ page.php
@@ -39,7 +39,7 @@
 <!-- Put this div tag to the place, where the Comments block will be -->
 <div id="vk_comments"></div>
 <script type="text/javascript">
-VK.Widgets.Comments("vk_comments", {limit: 10, width: "665", attach: "*"});
+VK.Widgets.Comments("vk_comments", {limit: 10, width: "665", attach: "*", autoPublish: 0});
 </script>
               
               
